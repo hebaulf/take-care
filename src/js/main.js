0039458110a595ecc5fcb0168f24ef42791a6d27
ArnaVala <arnavala@gmail.com>
2021-10-14 15:05:02 +0000

    create routes

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,15 @@
-console.log('mainJs');
+// SPA - routes - pathnames - views - History API
+
+const routes = {
+  // profile
+  '/signup': signup, // create profile
+  '/login': login,
+  '/profile': profile, // edit profile
+  // app
+  '/': dashboard, // display username
+  '/create': create,  //create new: todo, appointments, events
+  '/events': events, // view events - create events - assign events
+  '/journal': journal, // view events - create events - assign events
+  '/todos': todos // view events - create events - assign events
+}
+
